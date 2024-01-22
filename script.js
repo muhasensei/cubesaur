888347edf5e9c4edb09bb4ad258ce67e4e8eb41f
m.zhanuzakov <m.zhanuzakov@jusaninvest.kz>
2024-01-22 16:48:20 +0600

    added attempts & fixed restart issue

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const game = document.getElementById('game');
 const result = document.getElementById('result');
 const startBtn = document.getElementById('start-btn');
 const resultText = document.getElementById('resultText');
-
+const attemptsText = document.getElementById('attempts');
 
 const MAX_GAME_DURATION = 300000; // 5 mins
 const SPAWNING_INTERVAL = 3000;
@@ -11,15 +11,16 @@ const SPAWNING_INTERVAL = 3000;
 let attempts = 0;
 
 const startGame = () => {
-    console.log({attempts})
+    attempts++;
     const startTime = new Date().getTime();
+    attemptsText.textContent = `Attempt: ${attempts}`;
     startBtn.style.visibility = 'hidden';
     document.querySelectorAll('.block').forEach((block) => block.remove());
     resultText.style.visibility = 'hidden';
     resultText.textContent = '';
 
     const stopGame = () => {
-        clearInterval(spawning);
+        console.log('CALLED')
         startBtn.style.visibility = 'visible';
         startBtn.textContent = 'Restart Game';
 
@@ -66,8 +67,15 @@ const startGame = () => {
             const playerPosition = player.getBoundingClientRect();
             const blockPosition = block.getBoundingClientRect();
 
-            if (playerPosition.right > blockPosition.left && playerPosition.bottom > blockPosition.top) {
+            if (
+                blockPosition.left !== 0 &&
+                blockPosition.top !== 0 &&
+                playerPosition.right > blockPosition.left &&
+                playerPosition.bottom > blockPosition.top
+            ) {
                 clearInterval(checkIfBlockIsTouched);
+                clearInterval(spawning);
+                block.remove();
                 stopGame();
             } else {
                 block.addEventListener('animationend', () => {
